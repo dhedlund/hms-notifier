39f191eff0afdadc4e35d9df324a76ee89203458
Daniel Hedlund <daniel@digitree.org>
2011-06-09 03:20:12 -0700

    Initial Commit.  Newly generated rails 3.0.7 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HmsNotifier::Application.config.secret_token = '5d6a673c4c81b5d18e143083cd63f76aa7393ee62030c751e3084efe9882930136f8cb24f5479a5851b978ca0b50aaf0f1b0ce736159a6204e1a1a3bb0826ea8'
