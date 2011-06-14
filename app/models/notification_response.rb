86ceb89044b7a3a3828b2ab6dbae5b27842c5732
Daniel Hedlund <daniel@digitree.org>
2011-06-14 11:49:31 -0700

    NotificationResponse: update notification w/ notification status changes

--- app/models/notification_response.rb
+++ app/models/notification_response.rb
@@ -1,6 +1,22 @@
 class NotificationResponse < ActiveRecord::Base
   belongs_to :notification
 
+  after_save :update_notification
+
   validates :notification_id, :presence => true
   validates :status, :presence => true
+
+
+  protected
+
+  def update_notification
+    return true if status == notification.status
+    notification.status = status
+    notification.delivered_at = delivered_at
+
+    unless notification.save
+      notification.reload
+      false
+    end
+  end
 end
--- test/unit/notification_response_test.rb
+++ test/unit/notification_response_test.rb
@@ -5,10 +5,6 @@ class NotificationResponseTest < ActiveSupport::TestCase
     assert Factory.build(:notification_response).valid?
   end
 
-  test "should be invalid without a status" do
-    assert Factory.build(:notification_response, :status => nil).invalid?
-  end
-
   test "should be valid without an error_type" do
     assert Factory.build(:notification_response, :error_type => nil).valid?
   end
@@ -17,8 +13,41 @@ class NotificationResponseTest < ActiveSupport::TestCase
     assert Factory.build(:notification_response, :error_msg => nil).valid?
   end
 
-  test "should be valid without a delivery_date" do
+  test "should be valid without a delivered_at date" do
     assert Factory.build(:notification_response, :delivered_at => nil).valid?
+  end
+
+  #----------------------------------------------------------------------------#
+  # status:
+  #--------
+  test "should be invalid without a status" do
+    assert Factory.build(:notification_response, :status => nil).invalid?
+  end
+
+  test "unexpected status values should be valid" do
+    assert Factory.build(:notification_response, :status => 'EARTH_NOT_FOUND').valid?
+  end
+
+  #----------------------------------------------------------------------------#
+  # save:
+  #------
+  test "should update notification's status on save" do
+    response = Factory.create(:notification_response, :status => 'DELIVERED')
+    assert_equal Notification::DELIVERED, response.notification.status
+  end
+
+  test "should update notification's delivered_at value on save" do
+    response = Factory.create(:notification_response,
+      :status => 'DELIVERED',
+      :delivered_at => 1.day.ago
+    )
+    assert_equal response.delivered_at, response.notification.delivered_at
+  end
+
+  test "should not raise error when saving w/ invalid status" do
+    response = Factory.create(:notification_response, :status => 'EARTH_NOT_FOUND')
+    orig_status = response.notification.status
+    assert_nothing_raised { response.save! }
   end
 
   #----------------------------------------------------------------------------#
