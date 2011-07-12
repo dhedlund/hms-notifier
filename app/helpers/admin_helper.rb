4dc26d0cfad882d6be8b486af242197433460d2a
Daniel Hedlund <daniel@digitree.org>
2011-07-12 12:08:11 -0700

    Users: updated index and view page with new layout

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,9 +2,10 @@ module AdminHelper
   def primary_nav(selected=:dashboard)
     nav = [
       { :name => :dashboard,     :path => admin_path                   },
-      { :name => :enrollments,   :path => admin_enrollments_path       },
       { :name => :messages,      :path => admin_message_streams_path   },
+      { :name => :enrollments,   :path => admin_enrollments_path       },
       { :name => :notifications, :path => admin_notifications_path     },
+      { :name => :users,         :path => admin_users_path             },
     ]
 
     nav.each { |i| i[:title] ||= i[:name].to_s.titleize }
