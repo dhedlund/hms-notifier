ac85c1b4b2eaee1781f2989735628316612439d3
Daniel Hedlund <daniel@digitree.org>
2013-03-07 15:31:43 -0800

    Add some rails 3.{1,2} configs, update to use asset pipeline

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-HmsNotifier::Application.config.session_store :cookie_store, :key => '_hms-notifier_session'
+HmsNotifier::Application.config.session_store :cookie_store, :key => '_hms_notifier_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
